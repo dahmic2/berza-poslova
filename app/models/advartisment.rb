1a5d8e14740134132c621aec6b1cea97452e5bed
dahmic2 <dahmic2@etf.unsa.ba>
2014-03-21 12:50:12 +0100

    svasta po malo xD

--- app/models/advartisment.rb
+++ app/models/advartisment.rb
@@ -1,4 +1,7 @@
 class Advartisment < ActiveRecord::Base
+
+  validates_presence_of :title, :description, :published, :expire, :category, :on => :create
+
   belongs_to :employer, :class_name =>'Employer', :foreign_key => :employer_id
   belongs_to :category, :class_name =>'Category', :foreign_key => :category_id
 end
