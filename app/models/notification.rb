d6b8ed2cb68c0d985026272746d01ece410138eb
dahmic2 <dahmic2@etf.unsa.ba>
2014-03-26 15:38:47 +0100

    neke validacije

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,4 @@
 class Notification < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :category
+  belongs_to :user,  :class_name =>'User', :foreign_key => :user_ide
+  belongs_to :category,  :class_name =>'Category', :foreign_key => :category_id
 end
